a7cb5afba47ad2ff349a68e5af4e864482f8b9c6
Alisa03 <alisa033306@gmail.com>
2024-10-20 12:19:18 +0300

    fix error v1

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,7 +5,6 @@ import json from '../../server/db.json'
 import { Container, Title3 } from "../assets/styles/styledComponents";
 
 import Background from "../components/ui/Background";
-import Fon from "../assets/img/fon.webp"
 import ListMythology from "../components/layout/ListMythology";
 import Loading from "../components/ui/Loading";
 
@@ -15,7 +14,7 @@ const Home = () => {
     return (
         <Suspense fallback={<Loading />}>
             <div>
-                <Background fon={Fon} title="Сборник мифов&nbsp;" subtitle="и богов" />
+                <Background fon="/Fon.webp" title="Сборник мифов&nbsp;" subtitle="и богов" />
                 <Container>
                     <Title3>Мифологии</Title3>
                     <ListMythology data={mythologys} />
